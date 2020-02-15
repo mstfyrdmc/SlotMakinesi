6af221ef7a859288e729471574ed10346459c863
mstfyrdmc <45285832+AdamGiby@users.noreply.github.com>
2020-02-15 19:18:36 +0300

    SlotMachine

---
+++ WFASlotMachine/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace WFASlotMachine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnKoluCek = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pbResim3 = new System.Windows.Forms.PictureBox();
+            this.pbResim2 = new System.Windows.Forms.PictureBox();
+            this.pbResim1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbResim3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbResim2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbResim1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnKoluCek
+            // 
+            this.btnKoluCek.Enabled = false;
+            this.btnKoluCek.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnKoluCek.Location = new System.Drawing.Point(622, 40);
+            this.btnKoluCek.Name = "btnKoluCek";
+            this.btnKoluCek.Size = new System.Drawing.Size(150, 150);
+            this.btnKoluCek.TabIndex = 1;
+            this.btnKoluCek.Text = "Kolu Çek";
+            this.btnKoluCek.UseVisualStyleBackColor = true;
+            this.btnKoluCek.Click += new System.EventHandler(this.btnKoluCek_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(28, 247);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(187, 116);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Jeton Yatırma";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Oyun Bir Jetonla Çalışır.";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(46, 67);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Jeton yatır";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pbResim3
+            // 
+            this.pbResim3.Location = new System.Drawing.Point(431, 40);
+            this.pbResim3.Name = "pbResim3";
+            this.pbResim3.Size = new System.Drawing.Size(150, 150);
+            this.pbResim3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbResim3.TabIndex = 0;
+            this.pbResim3.TabStop = false;
+            // 
+            // pbResim2
+            // 
+            this.pbResim2.Location = new System.Drawing.Point(225, 40);
+            this.pbResim2.Name = "pbResim2";
+            this.pbResim2.Size = new System.Drawing.Size(150, 150);
+            this.pbResim2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbResim2.TabIndex = 0;
+            this.pbResim2.TabStop = false;
+            // 
+            // pbResim1
+            // 
+            this.pbResim1.Location = new System.Drawing.Point(22, 40);
+            this.pbResim1.Name = "pbResim1";
+            this.pbResim1.Size = new System.Drawing.Size(150, 150);
+            this.pbResim1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbResim1.TabIndex = 0;
+            this.pbResim1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnKoluCek);
+            this.Controls.Add(this.pbResim3);
+            this.Controls.Add(this.pbResim2);
+            this.Controls.Add(this.pbResim1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbResim3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbResim2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbResim1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbResim1;
+        private System.Windows.Forms.PictureBox pbResim2;
+        private System.Windows.Forms.PictureBox pbResim3;
+        private System.Windows.Forms.Button btnKoluCek;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WFASlotMachine/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFASlotMachine
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Random rnd = new Random();
+        int sayac = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sayac++;
+            btnKoluCek.Enabled = true;
+            button1.Enabled = false;
+            this.BackgroundImage = null;
+        }
+
+        private void btnKoluCek_Click(object sender, EventArgs e)
+        {
+            int resimSec1 = rnd.Next(0, 6);
+            int resimSec2 = rnd.Next(0, 6);
+            int resimSec3 = rnd.Next(0, 6);
+            
+            if (sayac >= 10)
+            {
+                int sayi = rnd.Next(0, 6);
+                switch (sayi)
+                {
+                    case 2:
+                        resimSec1 = resimSec2 = resimSec3 = rnd.Next(0,6);
+                        sayac = 0;
+                        break;
+                    default:
+                        resimSec1 = rnd.Next(0, 6);
+                        resimSec2 = rnd.Next(0, 6);
+                        resimSec3 = rnd.Next(0, 6);
+                        break;
+                }
+            }
+            switch (resimSec1)
+            {
+                case 0:
+                    pbResim1.Image = Properties.Resources.apple;
+                    break;
+                case 1:
+                    pbResim1.Image = Properties.Resources.avakado;
+                    break;
+                case 2:
+                    pbResim1.Image = Properties.Resources.banana;
+                    break;
+                case 3:
+                    pbResim1.Image = Properties.Resources.berry;
+                    break;
+                case 4:
+                    pbResim1.Image = Properties.Resources.orange;
+                    break;
+                case 5:
+                    pbResim1.Image = Properties.Resources.strawberry;
+                    break;
+            }
+            switch (resimSec2)
+            {
+                case 0:
+                    pbResim2.Image = Properties.Resources.apple;
+                    break;
+                case 1:
+                    pbResim2.Image = Properties.Resources.avakado;
+                    break;
+                case 2:
+                    pbResim2.Image = Properties.Resources.banana;
+                    break;
+                case 3:
+                    pbResim2.Image = Properties.Resources.berry;
+                    break;
+                case 4:
+                    pbResim2.Image = Properties.Resources.orange;
+                    break;
+                case 5:
+                    pbResim2.Image = Properties.Resources.strawberry;
+                    break;
+            }
+            switch (resimSec3)
+            {
+                case 0:
+                    pbResim3.Image = Properties.Resources.apple;
+                    break;
+                case 1:
+                    pbResim3.Image = Properties.Resources.avakado;
+                    break;
+                case 2:
+                    pbResim3.Image = Properties.Resources.banana;
+                    break;
+                case 3:
+                    pbResim3.Image = Properties.Resources.berry;
+                    break;
+                case 4:
+                    pbResim3.Image = Properties.Resources.orange;
+                    break;
+                case 5:
+                    pbResim3.Image = Properties.Resources.strawberry;
+                    break;
+            }
+            button1.Enabled = true;
+            btnKoluCek.Enabled = false;
+            if (pbResim1.Image == pbResim2.Image & pbResim2.Image == pbResim3.Image)
+            {
+                this.BackgroundImage = Properties.Resources.havaifisek;
+                MessageBox.Show("Kazandınız");
+            }
+            else
+            {
+                this.BackgroundImage = Properties.Resources.uzgun;
+                MessageBox.Show("Kaybettiniz");
+            }
+            
+
+        }
+    }
+}
